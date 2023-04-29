9055dd163ce2373ffeade43d9cc941905b5bbd78
Kollishon <a86421383@gmail.com>
2023-04-29 22:31:51 +0530

    UI change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,14 @@ import {main_body} from '../assets'
 const Hero = () => {
   return (
     <div className="">
-      <picture>
-      <img src={main_body} alt="main_body"  className="w-full" />
-      <h1 className="text-shadow: 0 0 25px black absolute text-center">Welcome <br /> To <br /> The <br /> Restaurant </h1>
+      <picture className=" w-40 h-40 rounded-full overflow-hidden">
+      <img src={main_body} alt="main_body"  className="w-full
+      md:w-50% h-50%" />
+      <h1 className="absolute w-full py-2.5 bottom-0
+       inset-x-0  
+        text-xs text-center 
+      leading-4"> <p>Welcome</p> <p>To</p> <p>The</p> 
+      <p>Restaurant</p> </h1>
       </picture>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,25 +12,48 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="shadow-md w-full fixed top-0 left-0 bg-black-gradient-2">
-      <div className="md:flex item-center justify-between py-4 md:px-10 px-7 font-serif">
-        <span className="md:static left-6 absolute text-3xl font-semibold text-gradient text-opacity-100 mr-2">Roo's</span>
-        <img src={open ? close : menu} onClick={() => setOpen((open) => !open)} alt="menu" className="w-[28px] h-[28px] md:hidden absolute right-6 object-contain z-10" />
+    <nav className="shadow-md w-full fixed top-0 
+    left-0 bg-black-gradient-2 p-1">
+      <div className="md:flex item-center 
+      justify-between py-3 md:px-10 px-7 font-serif">
+        <span className="md:static left-6 
+        absolute text-3xl font-semibold text-gradient 
+        text-opacity-100 mr-2 leading-[2]">Roo's</span>
+        <img src={open ? close : menu}
+         onClick={() => 
+         setOpen((open) => !open)} alt="menu"
+          className="w-[28px] h-[28px] md:hidden
+           absolute right-6 object-contain z-10" />
         <div
-          className={`md:flex md:items-center md:pb-0 pb-12 left-0 w-full float-right md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-20 opacity-100" : "top-[-490px] md:opacity-100 opacity-0"
+          className={`md:flex md:items-center 
+          md:pb-0 pb-10 left-0 w-full float-right
+           md:w-auto md:pl-0 pl-9 transition-all
+            duration-500 ease-in ${
+            open ? "top-20 opacity-100" : 
+            "top-[-490px] md:opacity-100 opacity-0"
           }`}
         >
           {links.map((links) => (
-            <div className="md:ml-8 text-gray-300 text-xl md:my-0 my-7">
-              <a href="links.link" className="hover:text-white duration-300">
+            <div key={links.id} className="md:ml-8 
+             text-gray-300 text-xl md:my-0 my-6">
+              <a href="links.link" 
+              className="hover:text-white duration-300">
                 {links.name}
               </a>
             </div>
           ))}
-          <div className="md:space-x-8 flex md:flex-row flex-col w-52 mx-auto text-white space-y-5">
-            <button className="bg-blue-600 font-extrabold p-1 transition duration-300 rounded hover:bg-blue-800 item md:ml-8">Sign in</button>
-            <button className="bg-blue-600 font-extrabold p-1 transition duration-300 hover:bg-blue-800 rounded">Log In</button>
+          <div className="md:space-x-8 
+          flex md:flex-row flex-col w-52 mx-auto
+           text-white  items-center
+           ">
+            <button className="bg-blue-600 
+            font-extrabold p-1 transition duration-300
+             rounded hover:bg-blue-800 item
+              md:ml-8">Sign in</button>
+            <button className="bg-blue-600 
+            font-extrabold  transition duration-300
+             hover:bg-blue-800 rounded
+             p-1 md:mt-0 mt-5">Log In</button>
           </div>
         </div>
       </div>
